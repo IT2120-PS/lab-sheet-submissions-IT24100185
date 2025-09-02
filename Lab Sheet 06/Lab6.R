f2d76b770a1c26bcbec51ddd3ac138a06a9f290c
IT24100185 <it24100185@my.sliit.lk>
2025-09-03 00:21:10 +0530

    Addlab6

---
+++ Lab Sheet 06/Lab6.R
@@ -0,0 +1,12 @@
+setwd("F:\\SLIIT\\Y2S1\\PS\\IT24100185_lab6")
+#1
+# I.  n = 50, p = 0.85
+##II
+pbinom(47, 50, 0.85)
+
+#2
+#I. number of calls per hour
+
+#II. Poisson Distribution -> lambda=12
+#III.
+dpois(15, 5)
